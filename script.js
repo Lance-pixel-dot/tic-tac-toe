78be0a1d324bfea550756461a5a91952faad69b9
Renier Lawrence Tuico <renierlawrencetuico143@gmail.com>
2025-01-21 18:31:48 +0800

    You can now play the game in the console.

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// function startGame(){
-// create players
-//     const player
-// make player choose between 'X' or 'O'
-// }
-
-const playRound = (() => {
-    //create board on console
+const startGame = ((playerOneMarker, playerTwoMarker, playerOneName, playerTwoName) => {
     const rows = 3;
     const columns = 3;
     const gameBoard = [];
@@ -13,21 +6,134 @@ const playRound = (() => {
     for(let i = 0; i < rows; i++){
         gameBoard[i] = [];
         for(let j = 0; j < columns; j++){
-            gameBoard[i][j] = 'X';
+            gameBoard[i][j] = '';
         }
     }
 
-    gameBoard[0][1] = 'O';
-    gameBoard[1][0] = 'O';
-    gameBoard[1][2] = 'O';
-    gameBoard[2][1] = 'O';
-
     console.log(gameBoard);
 
-    //make sure both players take turns
+    let keepRunning = true;
 
-    //make players choose what row and column to place their 'X' or 'O'
+ gamePlayActionOne();
 
-    //then output the result on the console
+function gamePlayActionOne(){
 
+    const playerOneRow = prompt('PLayer 1 enter the row where you want to place your Marker');
+    const playerOneColumn = prompt('Player 1 enter the column where you want to place your Marker');
+
+    gameBoard[playerOneRow][playerOneColumn] = playerOneMarker;
+
+    winConditionChecker();
+
+}
+function gamePlayActionTwo(){
+    
+    const playerTwoRow = prompt('Player 2 enter the row where you want to place your Marker');
+    const playerTwoColumn = prompt('Player 2 enter the column where you want to place your Marker');
+
+    gameBoard[playerTwoRow][playerTwoColumn] = playerTwoMarker;
+
+    winConditionChecker();
+
+}
+    console.log(gameBoard);
+    
+function winConditionChecker(){
+    if(
+        //player one winning conditions
+        gameBoard[0][0] == playerOneMarker && gameBoard[0][1] == playerOneMarker && gameBoard[0][2] == playerOneMarker ||
+        gameBoard[1][0] == playerOneMarker && gameBoard[1][1] == playerOneMarker && gameBoard[1][2] == playerOneMarker ||
+        gameBoard[2][0] == playerOneMarker && gameBoard[2][1] == playerOneMarker && gameBoard[2][2] == playerOneMarker ||
+        gameBoard[0][0] == playerOneMarker && gameBoard[1][1] == playerOneMarker && gameBoard[2][2] == playerOneMarker ||
+        gameBoard[0][2] == playerOneMarker && gameBoard[1][1] == playerOneMarker && gameBoard[2][0] == playerOneMarker
+    ){
+        keepRunning = false;
+        console.log(keepRunning);
+        console.log(`${playerOneName} Wins!`);
+        return;
+    }else if(
+        //player two winning conditions 
+        gameBoard[0][0] == playerTwoMarker && gameBoard[0][1] == playerTwoMarker && gameBoard[0][2] == playerTwoMarker ||
+        gameBoard[1][0] == playerTwoMarker && gameBoard[1][1] == playerTwoMarker && gameBoard[1][2] == playerTwoMarker ||
+        gameBoard[2][0] == playerTwoMarker && gameBoard[2][1] == playerTwoMarker && gameBoard[2][2] == playerTwoMarker ||
+        gameBoard[0][0] == playerTwoMarker && gameBoard[1][1] == playerTwoMarker && gameBoard[2][2] == playerTwoMarker ||
+        gameBoard[0][2] == playerTwoMarker && gameBoard[1][1] == playerTwoMarker && gameBoard[2][0] == playerTwoMarker 
+    ){
+        keepRunning = false;
+        console.log(keepRunning);
+        console.log(`${playerTwoName} Wins!`);
+        return;
+    }else if(
+        //draw condition
+        gameBoard[0][0] != '' && gameBoard[0][1] != '' && gameBoard[0][2] != '' &&
+        gameBoard[1][0] != '' && gameBoard[1][1] != '' && gameBoard[1][2] != '' &&
+        gameBoard[2][0] != '' && gameBoard[2][1] != '' && gameBoard[2][2] != '' &&
+        gameBoard[0][0] != '' && gameBoard[1][1] != '' && gameBoard[2][2] != '' &&
+        gameBoard[0][2] != '' && gameBoard[1][1] != '' && gameBoard[2][0] != ''
+    ){
+         keepRunning = false;
+         console.log(keepRunning);
+         console.log('Draw!');
+         return;
+    }else if(keepRunning == true){
+        keepRunning = 'stop';
+        gamePlayActionTwo();
+    }else if(keepRunning === 'stop'){
+        keepRunning = true;
+        gamePlayActionOne();
+    }
+}
+
+});
+
+
+const createPlayer = (name, marker) => {
+    return{ name, marker };
+}
+
+
+const getPlayer = (() => {
+
+    let playerOneName = prompt('What\'s Player 1 name?');
+    let playerOneMarker = prompt('What\'s Player 1 Marker?', 'X or O');
+
+    let playerTwoName;
+    let playerTwoMarker;
+
+    playerOneMarker = playerOneMarker.toUpperCase();
+
+    if(playerOneMarker != 'X' && playerOneMarker != 'O'){
+
+        alert('You can only choose between \'X\' or \'O\'');
+
+    }else if(playerOneMarker == 'X'){
+
+        alert('Then player 2 Marker will be \'O\'');
+        playerTwoName = prompt('And what\'s Player 2 name?');
+        playerTwoMarker = 'O';
+
+        const playerOne = createPlayer(playerOneName, playerOneMarker);
+        const playerTwo = createPlayer(playerTwoName, playerTwoMarker);
+    
+        console.log(playerOne);
+        console.log(playerTwo);
+    
+        startGame(playerOneMarker, playerTwoMarker, playerOneName, playerTwoName);
+
+    }else if(playerOneMarker == 'O'){
+
+        alert('Then player 2 Marker will be \'X\'');
+        playerTwoName = prompt('And what\'s Player 2 name?');
+        playerTwoMarker = 'X';
+
+        const playerOne = createPlayer(playerOneName, playerOneMarker);
+        const playerTwo = createPlayer(playerTwoName, playerTwoMarker);
+    
+        console.log(playerOne);
+        console.log(playerTwo);
+
+        startGame(playerOneMarker, playerTwoMarker, playerOneName, playerTwoName);
+
+    }
+    
 })();
